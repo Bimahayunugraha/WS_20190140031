30dc4f02eef86d52b2104df7938b1fabdeee6d1a
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2021-12-22 23:06:44 +0700

    first commit

---
+++ assesment.031.a/src/main/java/ti/umy/web/services/assesment1/a/Application.java
@@ -0,0 +1,13 @@
+package ti.umy.web.services.assesment1.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ assesment.031.a/src/main/java/ti/umy/web/services/assesment1/a/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package ti.umy.web.services.assesment1.a.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
